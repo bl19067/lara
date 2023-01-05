6a0daf7027bcc92edca1331bb0d05d22872489e7
bl19067 <bl19067@cc.it-hiroshima.ac.jp>
2023-01-05 18:46:04 +0900

    lara

--- lara/app/Http/Controllers/Controller.php
+++ lara/app/Http/Controllers/Controller.php
@@ -2,12 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
-class Controller extends BaseController
-{
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+#class Controller extends BaseController
+#{
+       #use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+#}
+class Controller extends Controller{
+       public function index() {
+              return <<<EOF
+<html>
+<head>
+<title>hogehoge</title>
+<style>
+body {font-size:16pt; color:#999;}
+h1 { font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px;}
+</style>
+</head>
+<body>
+<h1>hogehoge</h1>
+<p>Hello!</p>
+<p>{$msg}</p>
+<p>sample hogeControllr</p>
+<h2>index</h2>
+</body>
+</html>
+EOF;
+       }
 }
